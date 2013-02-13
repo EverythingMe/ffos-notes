544a1cfc16e3f1728b66393d75b45c25f17147cc
Matias Crivolotti <matioav@gmail.com>
2013-02-13 10:58:51 +0200

    html to enml conversion

--- js-binary/notes.js
+++ js-binary/notes.js
@@ -1,5 +1,45 @@
 function $(i){return document.getElementById(i)}
 
+var ignoreTags = [
+	'applet',
+    'base',
+    'basefont',
+    'bgsound',
+    'blink',
+    'body',
+    'button',
+    'dir',
+    'embed',
+    'fieldset',
+    'form',
+    'frame',
+    'frameset',
+    'head',
+    'html',
+    'iframe',
+    'ilayer',
+    'isindex',
+    'label',
+    'layer,',
+    'legend',
+    'link',
+    'marquee',
+    'menu',
+    'meta',
+    'noframes',
+    'noscript',
+    'object',
+    'optgroup',
+    'option',
+    'param',
+    'plaintext',
+    'script',
+    'select',
+    'style',
+    'textarea',
+    'xml'
+];
+
 var EVERNOTE_SERVER = "https://sandbox.evernote.com",
 	OAUTH_CONSUMER_KEY = "israhack",
 	OAUTH_CONSUMER_SECRET = "76034784d7dd7aa5",
@@ -16,11 +56,14 @@ var EVERNOTE_SERVER = "https://sandbox.evernote.com",
 var tmp_oauth_token = "",
 	oauth_verifier = "",
 	oauth_token = "",
-	noteStoreUrl = "";
+	noteStoreUrl = "",
+	shardUrl = "";
 
 var noteStoreTransport,
 	noteStoreProtocol,
 	noteStore;
+
+var currentNote;
 
 var processXHR = function(url, method, callback) {
 	var xhr = new XMLHttpRequest({mozSystem: true});
@@ -110,10 +153,13 @@ var getAccessToken = function() {
 		$('authResult').innerHTML = JSON.stringify(responseData, true, 4);
 		oauth_token = responseData['oauth_token'];
 		noteStoreUrl = responseData['edam_noteStoreUrl'];
+		shardUrl = responseData['edam_webApiUrlPrefix'];
+		console.log(responseData);
 
 		var expires = new Date(responseData['edam_expires']).toString();
 		document.cookie = 'oauth_token='+oauth_token+';expires='+expires;
 		document.cookie = 'noteStoreUrl='+noteStoreUrl+';expires='+expires+';';
+		document.cookie = 'shardUrl='+shardUrl+';expires='+expires+';';
 		finishAuth();
 	});
 };
@@ -132,16 +178,19 @@ window.onload = function() {
 	$('fetchNotebooks').addEventListener('click', fetchNotebooks);
 	$('fetchNotebook').addEventListener('click', getNotebook);
 	$('fetchNote').addEventListener('click', getNote);
-	$('fetchResource').addEventListener('click', getResource);
+	$('saveNote').addEventListener('click', saveNote);
 
-	document.cookie = 'oauth_token=S%3Ds1%3AU%3D5a3fd%3AE%3D144065d8a8e%3AC%3D13caeac5e8e%3AP%3D185%3AA%3Disrahack%3AH%3D354f2adcef13d54e224b16408daea27d';
-	document.cookie = 'noteStoreUrl=https%3A%2F%2Fsandbox.evernote.com%2Fshard%2Fs1%2Fnotestore';
+	// document.cookie = 'oauth_token=S%3Ds1%3AU%3D5a3fd%3AE%3D144065d8a8e%3AC%3D13caeac5e8e%3AP%3D185%3AA%3Disrahack%3AH%3D354f2adcef13d54e224b16408daea27d';
+	// document.cookie = 'noteStoreUrl=https%3A%2F%2Fsandbox.evernote.com%2Fshard%2Fs1%2Fnotestore';
+	// document.cookie = 'shardUrl=https%3A%2F%2Fsandbox.evernote.com%2Fshard%2Fs1%2F';
 	if ((new RegExp("(?:^|;\\s*)" + escape('oauth_token').replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie) && (new RegExp("(?:^|;\\s*)" + escape('noteStoreUrl').replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie)) {
 		oauth_token = document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape('oauth_token').replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1");
 		noteStoreUrl = document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape('noteStoreUrl').replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1");
+		shardUrl = document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape('shardUrl').replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1");
 		var data = {
 			oauth_token : oauth_token,
-			noteStoreUrl : noteStoreUrl
+			noteStoreUrl : noteStoreUrl,
+			shardUrl : shardUrl
 		};
 		$('authResult').innerHTML = JSON.stringify(data, true, 4);
 		finishAuth();
@@ -184,29 +233,113 @@ function getNote() {
 	}
 	var callback = {
 		onSuccess: function(note) {
-			$("notecontent").innerHTML = note.content;
+			currentNote = note;
+			var hashMap = {};
+			var resMap = {};
+			for (var r in note.resources) {
+				var key = "",
+					value = "",
+					bytes = [];
+				for (var i in note.resources[r].data.bodyHash) {
+				    key += String("0123456789abcdef".substr((note.resources[r].data.bodyHash[i] >> 4) & 0x0F,1)) + "0123456789abcdef".substr(note.resources[r].data.bodyHash[i] & 0x0F,1);
+				}
+				for (var i in note.resources[r].data.body) {
+				    value += String("0123456789abcdef".substr((note.resources[r].data.body[i] >> 4) & 0x0F,1)) + "0123456789abcdef".substr(note.resources[r].data.body[i] & 0x0F,1);
+				}
+				for(var i=0; i< value.length-1; i+=2){
+				    bytes.push(parseInt(value.substr(i, 2), 16));
+				}
+				hashMap[key] = window.btoa(String.fromCharCode.apply(String, bytes));
+				resMap[key] = note.resources[r].guid;
+			}
+			var html = "";
+			html = enml.HTMLOfENML(note.content,hashMap);
+			// html = note.content;
 			console.log(note.content);
-			$("getNote").innerHTML = JSON.stringify(note.resources, true, 4)+"\n";
+			$("notecontent").innerHTML = html;
+			$("getNote").innerHTML = note.content;
+			// $("getNote").innerHTML = JSON.stringify(hashMap, true, 4)+"\n";
+			// $("getNote").innerHTML += "\n\n\n"+JSON.stringify(resMap, true, 4)+"\n";
 		},
 		onFailure: function(error) {
 			alert("getNote received error: "+error);
 		}
 	};
-	noteStore.getNote(decodeURIComponent(oauth_token), guid, true, true, false, false, callback.onSuccess,callback.onFailure);
+	noteStore.getNote(decodeURIComponent(oauth_token), guid, true, true, true, true, callback.onSuccess,callback.onFailure);
 }
-function getResource() {
-	var guid = $("resourceguid").value;
-	if (guid.length == 0) {
-		alert('You must enter a note guid');
-		return;
-	}
+function saveNote() {
+	var html = $('notecontent').innerHTML;
+	var writer = new XMLWriter;
+	writer.startDocument('1.0', 'UTF-8', false);
+	writer.write('<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">');
+	writer.startElement('en-note');
+	var parser = new SaxParser(function(cb) {
+		cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
+			if (ignoreTags.indexOf(elem) == -1) {
+				if(elem == 'input') {
+					var type = null;
+					var checked = false;
+					if(attrs) attrs.forEach(function(attr) {
+						if(attr[0] == 'type') type = attr[1];
+						if(attr[0] == 'checked') checked = attr[1];
+					});
+					if (type == 'checkbox') {
+						writer.startElement('en-todo');
+						if (checked) {
+							writer.writeAttribute('checked', checked);
+						}
+						writer.endElement();
+						return;
+					}
+				} else if(elem == 'img'){
+					var type = null;
+					var hash = null;
+					var width = 0;
+					var height = 0;
+
+					// console.log(attrs);
+					if(attrs) attrs.forEach(function(attr, i) {
+						if (attr[0] == 'type') type = attr[1];
+						if (attr[0] == 'src') delete attrs[i];
+					});
+
+					if(!type.match('image')) return;
+					writer.startElement('en-media');
+					if(attrs) attrs.forEach(function(attr) {
+						writer.writeAttribute(attr[0], attr[1]);
+					});
+					writer.endElement();
+					return;
+				} else {
+					writer.startElement(elem);
+				}
+
+				if(attrs) attrs.forEach(function(attr) {
+					writer.writeAttribute(attr[0], attr[1]);
+				});
+			}
+		});
+		cb.onEndElementNS(function(elem, prefix, uri) {
+			if (ignoreTags.indexOf(elem) == -1) {
+				writer.endElement();
+			}
+		});
+
+		cb.onCharacters(function(chars) {
+			writer.text(chars);
+		});
+	});
+	parser.parseString(html);
+	writer.endElement('en-note');
+	currentNote.content = writer.toString();
+	$("putNote").innerHTML = writer.toString();
 	var callback = {
-		onSuccess: function(resource) {
-			$("getResource").innerHTML = JSON.stringify(resource, true, 4)+"\n";
+		onSuccess: function(note) {
+			$("updateNote").innerHTML += JSON.stringify(note, true, 4)+"\n";
 		},
 		onFailure: function(error) {
-			alert("getResource received error: "+error);
+			alert("updateNote received error: "+error);
 		}
 	};
-	noteStore.getResource(decodeURIComponent(oauth_token), guid, true, false, true, true, callback.onSuccess, callback.onFailure);
+	noteStore.updateNote(decodeURIComponent(oauth_token), currentNote, callback.onSuccess, callback.onFailure);
 }
